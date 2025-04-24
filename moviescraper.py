c2d4e54ad8c1ff080c4962788f5e6895f6bb7634
VJHARAN <haranvj1997@gmail.com>
2025-04-24 18:08:05 +0530

    movie scraper fully working

--- moviescraper.py
+++ moviescraper.py
@@ -1,15 +1,26 @@
 import requests as req
 from bs4 import BeautifulSoup
-    
+import re
+
 url="https://www.1tamilmv.fi/"
 page=req.get(url)
 
+user_input=input("Input movie name or year to search: ")
 soup=BeautifulSoup(page.content,'html.parser')
 
 movies_container=soup.find(id='elCmsPageWrap')
 
-movies_texts=movies_container.find_all('span', string=lambda text: 'rips' not in text.lower() and 'malayalam' in text.lower() )
+movies_text=movies_container.find_all('span',string=lambda text: user_input in text.lower())
+parent_list=[span_element.parent for span_element in movies_text]
+for item in parent_list:
+    title=item.find('span')
+    print(title.text)
+    links=item.find_all('a',href=lambda text : '1tamilmv' in text.lower())
+    for link in links:
+        link_url = link["href"]
+        print(f"Open here: {link_url}"+"\n"*2)
+  
+ 
 
-for m in movies_texts:
-    print(m.text)
-    print("\n")
+
+
